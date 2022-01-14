6ae86c75a429f2e82b5a3d13603c8d607e9ab2a2
igorgaev2014 <igorgaev2014@gmail.com>
2022-01-14 15:04:45 +0300

    Исправил название константы

--- src/mock/comment.js
+++ src/mock/comment.js
@@ -1,10 +1,10 @@
 import { generateData, getRandomInteger, generateDate } from '../utils/common.js';
-import { commentAuthors, commentDescriptions, EMOTIONS } from '../consts.js';
+import { commentAuthors, commentDescriptions, commentEmotions } from '../consts.js';
 
 export const generateComment = () => ({
   id: getRandomInteger(0, 100),
   author: generateData(commentAuthors),
   comment: generateData(commentDescriptions),
   date: generateDate(),
-  emotion: generateData(EMOTIONS),
+  emotion: generateData(commentEmotions),
 });
